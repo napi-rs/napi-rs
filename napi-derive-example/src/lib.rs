0f28cc7bdf417a92c12021f642c47fb27c67a2ee
LongYinan <longyinan.brooklyn@bytedance.com>
2020-04-26 16:58:11 +0800

    doc: add napi-derive example and document

--- napi-derive-example/src/lib.rs
+++ napi-derive-example/src/lib.rs
@@ -3,7 +3,8 @@ extern crate napi_rs as napi;
 #[macro_use]
 extern crate napi_derive;
 
-use napi::{Any, Env, Error, Object, Result, Status, Value, CallContext};
+use napi::{Any, Env, Error, Object, Result, Status, Value, CallContext, Number};
+use std::convert::TryInto;
 
 register_module!(test_module, init);
 
@@ -15,12 +16,31 @@ fn init<'env>(
     "testThrow",
     env.create_function("testThrow", test_throw)?,
   )?;
+
+  exports.set_named_property(
+    "fibonacci",
+    env.create_function("fibonacci", fibonacci)?,
+  )?;
   Ok(None)
 }
 
 #[js_function]
 fn test_throw<'a>(
-  ctx: CallContext,
+  _ctx: CallContext,
 ) -> Result<Value<'a, Any>> {
   Err(Error::new(Status::GenericFailure))
+}
+
+#[js_function]
+fn fibonacci<'env>(ctx: CallContext<'env>) -> Result<Value<'env, Number>> {
+  let n = ctx.get::<Number>(0)?.try_into()?;
+  ctx.env.create_int64(fibonacci_native(n))
+}
+
+#[inline]
+fn fibonacci_native(n: i64) -> i64 {
+  match n {
+    1 | 2 => 1,
+    _ => fibonacci_native(n - 1) + fibonacci_native(n - 2)
+  }
 }
---
+++ napi-derive/README.md
@@ -0,0 +1,23 @@
+# napi-derive
+
+## js_function
+
+```rust
+use napi_rs::{Result, Value, CallContext, Number};
+use napi_derive::js_function;
+use std::convert::TryInto;
+
+#[js_function]
+fn fibonacci<'env>(ctx: CallContext<'env>) -> Result<Value<'env, Number>> {
+  let n = ctx.get::<Number>(0)?.try_into()?;
+  ctx.env.create_int64(fibonacci_native(n))
+}
+
+#[inline]
+fn fibonacci_native(n: i64) -> i64 {
+  match n {
+    1 | 2 => 1,
+    _ => fibonacci_native(n - 1) + fibonacci_native(n - 2)
+  }
+}
+```
