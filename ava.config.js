7992260eec375199975701499808d105ac898be1
LongYinan <lynweklm@gmail.com>
2020-09-22 00:19:26 +0800

    chore: upgrade deps

--- ava.config.js
+++ ava.config.js
@@ -2,6 +2,9 @@ const configuration = {
   extensions: ['ts', 'tsx'],
   files: ['test_module/__test__/**/*.spec.ts'],
   require: ['@swc-node/register'],
+  environmentVariables: {
+    SWC_NODE_PROJECT: './test_module/tsconfig.json',
+  },
 }
 
 if (parseInt(process.versions.napi, 10) < 4) {
