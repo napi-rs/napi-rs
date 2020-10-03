e713d647f546fd5142f1f37c8db11a51d234b7f5
LongYinan <lynweklm@gmail.com>
2020-10-03 14:57:19 +0800

    test(napi): add JsTimeout specs

--- test_module/__test__/global.spec.ts
+++ test_module/__test__/global.spec.ts
@@ -7,11 +7,21 @@ function wait(delay: number) {
   return new Promise((resolve) => setTimeout(resolve, delay))
 }
 
+const delay = 100
+
 test('should setTimeout', async (t) => {
   const handler = Sinon.spy()
-  const delay = 100
   bindings.setTimeout(handler, delay)
   t.is(handler.callCount, 0)
   await wait(delay + 10)
   t.is(handler.callCount, 1)
+})
+
+test('should clearTimeout', async (t) => {
+  const handler = Sinon.spy()
+  const timer = setTimeout(() => handler(), delay)
+  t.is(handler.callCount, 0)
+  bindings.clearTimeout(timer)
+  await wait(delay + 10)
+  t.is(handler.callCount, 0)
 })
