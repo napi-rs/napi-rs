b651be1becb33a4889ab93816045307990b90bcb
LongYinan <lynweklm@gmail.com>
2020-10-13 22:58:29 +0800

    ci: pass tests on the freebsd platform

--- ava.config.js
+++ ava.config.js
@@ -1,7 +1,15 @@
+import { platform } from 'os'
+
+const platformName = platform()
+
 const configuration = {
   extensions: ['ts', 'tsx'],
   files: ['test_module/__test__/**/*.spec.ts'],
-  require: ['@swc-node/register'],
+  require: [
+    platformName === 'freebsd'
+      ? 'ts-node/register/transpile-only'
+      : '@swc-node/register',
+  ],
   environmentVariables: {
     SWC_NODE_PROJECT: './test_module/tsconfig.json',
   },
