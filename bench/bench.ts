f03ada59df0774b0b9d7dd031d5f1efb04fc7ef9
LongYinan <lynweklm@gmail.com>
2020-10-14 00:18:12 +0800

    ci: setup benchmark action

--- bench/bench.ts
+++ bench/bench.ts
@@ -1,11 +1,27 @@
+import { promises as fs } from 'fs'
+import { join } from 'path'
+
+import { Summary } from 'benny/lib/internal/common-types'
+
 import { benchAsync } from './async'
 import { benchNoop } from './noop'
 import { benchPlus } from './plus'
 
 async function run() {
-  await benchNoop()
-  await benchPlus()
-  await benchAsync()
+  const output = [await benchNoop(), await benchPlus(), await benchAsync()]
+    .map(formatSummary)
+    .join('\n')
+
+  await fs.writeFile(join(process.cwd(), 'bench.txt'), output, 'utf8')
+}
+
+function formatSummary(summary: Summary): string {
+  return summary.results
+    .map(
+      (result) =>
+        `${summary.name}#${result.name} x ${result.ops} ops/sec ±${result.margin}% (${result.samples} runs sampled)`,
+    )
+    .join('\n')
 }
 
 run().catch((e) => {
