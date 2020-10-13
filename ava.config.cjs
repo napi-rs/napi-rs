0f36a8a6cbf1ada3f4dcd1b429cdd08b315ed458
LongYinan <lynweklm@gmail.com>
2020-10-14 00:08:22 +0800

    feat: support freebsd in napi build command

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,17 +1,13 @@
-import { platform } from 'os'
-
-const platformName = platform()
-
 const configuration = {
   extensions: ['ts', 'tsx'],
   files: ['test_module/__test__/**/*.spec.ts'],
   require: [
-    platformName === 'freebsd'
+    require('os').platform() === 'freebsd'
       ? 'ts-node/register/transpile-only'
       : '@swc-node/register',
   ],
   environmentVariables: {
-    SWC_NODE_PROJECT: './test_module/tsconfig.json',
+    TS_NODE_PROJECT: './test_module/tsconfig.json',
   },
 }
 
@@ -19,4 +15,4 @@ if (parseInt(process.versions.napi, 10) < 4) {
   configuration.compileEnhancements = false
 }
 
-export default configuration
+module.exports = configuration
