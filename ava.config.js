3d7d3b78e49800479fafc01b3f15dff44ff1d522
LongYinan <longyinan.brooklyn@bytedance.com>
2020-10-14 16:15:20 +0800

    feat: support linux aarch64

--- ava.config.js
+++ ava.config.js
@@ -1,18 +1,15 @@
 const configuration = {
   extensions: ['ts', 'tsx'],
   files: ['test_module/__test__/**/*.spec.ts'],
-  require: [
-    require('os').platform() === 'freebsd'
-      ? 'ts-node/register/transpile-only'
-      : '@swc-node/register',
-  ],
+  require: ['ts-node/register/transpile-only'],
   environmentVariables: {
     TS_NODE_PROJECT: './test_module/tsconfig.json',
   },
+  timeout: '1m',
 }
 
 if (parseInt(process.versions.napi, 10) < 4) {
   configuration.compileEnhancements = false
 }
 
-module.exports = configuration
+export default configuration
