dcc22ad46cc7dc05a6278de6013b7b00a92030e7
adumbidiot <nathaniel.daniel23@outlook.com>
2020-11-05 14:08:30 -0800

    Refactor napi3 ci to use utilize ci helper script to run `cargo check`

--- ci-scripts/index.js
+++ ci-scripts/index.js
@@ -1,5 +1,7 @@
 const childProcess = require('child_process')
 const yargs = require('yargs')
+const toml = require('toml')
+const fs = require('fs')
 
 let napiVersion = process.env['USE_NAPI_VERSION']
 
@@ -7,10 +9,33 @@ if (!napiVersion) {
   throw 'Missing `USE_NAPI_VERSION`'
 }
 
-const argv = yargs.command('build', 'Builds a module', {}, build).argv
+const argv = yargs
+  .command('build', 'Builds a module', {}, build)
+  .command(
+    'cargo-check-all',
+    'Runs cargo check on all crates',
+    {},
+    cargoCheckAll,
+  ).argv
 
 function build(args) {
   childProcess.execSync(`cargo build --features=napi${napiVersion}`)
 }
 
-childProcess.execSync('node ../scripts/index.js build')
+function cargoCheckAll(args) {
+  let topLevelTomlString = fs.readFileSync('./Cargo.toml')
+  let topLevelToml = toml.parse(topLevelTomlString)
+  let workspaceMembers = topLevelToml.workspace.members
+
+  for (let i = 0; i < workspaceMembers.length; i++) {
+    let member = workspaceMembers[i]
+    let command = `cargo check --manifest-path ${member}/Cargo.toml --bins --examples --tests -vvv`
+
+    if (member === './napi')
+      command += ` --no-default-features --features=napi${napiVersion}`
+
+    console.log('Running: ' + command)
+
+    childProcess.execSync(command)
+  }
+}
