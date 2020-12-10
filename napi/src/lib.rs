8513e4f79670c6bd783959686fe4bdb6060ea340
LongYinan <lynweklm@gmail.com>
2020-12-10 14:26:53 +0800

    docs: napi6 should be napi7

--- napi/src/lib.rs
+++ napi/src/lib.rs
@@ -6,7 +6,7 @@
 //!
 //! ## Feature flags
 //!
-//! ### napi1 ~ napi6
+//! ### napi1 ~ napi7
 //!
 //! Because `NodeJS` N-API has versions. So there are feature flags to choose what version of `N-API` you want to build for.
 //! For example, if you want build a library which can be used by `node@10.17.0`, you should choose the `napi5` or lower.
