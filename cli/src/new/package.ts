6fe60f69c16a1f435bc03b57c820da3077b234fd
LongYinan <lynweklm@gmail.com>
2020-12-16 00:04:11 +0800

    feat(cli): new project command

---
+++ cli/src/new/package.ts
@@ -0,0 +1,56 @@
+import { DefaultPlatforms } from '../parse-triple'
+
+export const createPackageJson = (
+  name: string,
+  binaryName: string,
+  targets: string[],
+) => {
+  const pkgContent = {
+    name,
+    napi: {
+      name: binaryName,
+    },
+    dependencies: {
+      '@node-rs/helper': '^1.0.0',
+    },
+    devDependencies: {
+      '@napi-rs/cli': '^1.0.0',
+    },
+    engines: {
+      node: '>= 10',
+    },
+    scripts: {
+      artifacts: 'napi artifacts',
+      build: 'napi build --platform --release',
+      'build:debug': 'napi build --platform',
+      prepublishOnly: 'napi prepublish -t npm',
+      version: 'napi version',
+    },
+  }
+
+  const triples: any = {}
+
+  const defaultTargetsSupported = DefaultPlatforms.every((p) =>
+    targets!.includes(p.raw),
+  )
+
+  const isOnlyDefaultTargets =
+    targets.length === 3 &&
+    DefaultPlatforms.every((p) => targets.includes(p.raw))
+
+  if (!isOnlyDefaultTargets) {
+    if (!defaultTargetsSupported) {
+      triples.defaults = false
+      triples.additional = targets
+    } else {
+      triples.additional = targets.filter(
+        (t) => !DefaultPlatforms.map((p) => p.raw).includes(t),
+      )
+    }
+  }
+
+  // @ts-expect-error
+  pkgContent.napi.triples = triples
+
+  return pkgContent
+}
