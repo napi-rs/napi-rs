c4734e23a18c62bd18e60f336ed4081981a7e597
LongYinan <lynweklm@gmail.com>
2020-12-17 00:27:33 +0800

    feat(napi): set and get instance data

--- test_module/src/napi6/mod.rs
+++ test_module/src/napi6/mod.rs
@@ -1,8 +1,10 @@
 use napi::{JsObject, Result};
 
 mod bigint;
+mod instance;
 
 use bigint::*;
+use instance::*;
 
 pub fn register_js(exports: &mut JsObject) -> Result<()> {
   exports.create_named_method("testCreateBigintFromI64", test_create_bigint_from_i64)?;
@@ -13,5 +15,9 @@ pub fn register_js(exports: &mut JsObject) -> Result<()> {
   exports.create_named_method("testGetBigintI64", test_get_bigint_i64)?;
   exports.create_named_method("testGetBigintU64", test_get_bigint_u64)?;
   exports.create_named_method("testGetBigintWords", test_get_bigint_words)?;
+
+  exports.create_named_method("setInstanceData", set_instance_data)?;
+  exports.create_named_method("getInstanceData", get_instance_data)?;
+  exports.create_named_method("getWrongTypeInstanceData", get_wrong_type_instance_data)?;
   Ok(())
 }
