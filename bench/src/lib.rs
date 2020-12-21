db1dfb4231dd14ab3a5ac6699fc27673760958a3
LongYinan <lynweklm@gmail.com>
2020-12-21 16:10:30 +0800

    fix(napi): memory issues in create_external_buffer

--- bench/src/lib.rs
+++ bench/src/lib.rs
@@ -3,7 +3,16 @@ extern crate napi_derive;
 
 use napi::{JsObject, Result};
 
+#[cfg(all(unix, not(target_env = "musl"), not(target_arch = "aarch64")))]
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[cfg(windows)]
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 mod async_compute;
+mod buffer;
 mod noop;
 mod plus;
 
@@ -12,6 +21,7 @@ fn init(mut exports: JsObject) -> Result<()> {
   exports.create_named_method("noop", noop::noop)?;
 
   async_compute::register_js(&mut exports)?;
+  buffer::register_js(&mut exports)?;
   plus::register_js(&mut exports)?;
 
   Ok(())
