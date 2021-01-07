58d4634dacca673a83bf49cfb2dd15c7f8444865
forehalo <forehalo@gmail.com>
2021-01-07 23:26:41 +0800

    fix(cli): fix random node process got killed issue

--- cli/src/utils.ts
+++ cli/src/utils.ts
@@ -1,6 +1,8 @@
-import { readFile, writeFile, exists } from 'fs'
+import { readFile, writeFile, exists, copyFile, unlink } from 'fs'
 import { promisify } from 'util'
 
 export const readFileAsync = promisify(readFile)
 export const writeFileAsync = promisify(writeFile)
 export const existsAsync = promisify(exists)
+export const unlinkAsync = promisify(unlink)
+export const copyFileAsync = promisify(copyFile)
