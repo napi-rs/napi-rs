74e6cf2916a15909203760da456beb7c0d7c13f0
Chris Nixon <chris.nixon@sigma.me.uk>
2021-04-01 23:32:01 +0100

    Don't try to use ureq on musl targets

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -1,10 +1,23 @@
 mod macos;
-mod windows;
+
+cfg_if::cfg_if! {
+  if #[cfg(not(target_env = "musl"))] {
+    mod windows;
+  }
+}
 
 pub fn setup() {
   match std::env::var("CARGO_CFG_TARGET_OS").as_deref() {
     Ok("macos") => macos::setup(),
-    Ok("windows") => windows::setup(),
+    Ok("windows") => {
+      cfg_if::cfg_if! {
+        if #[cfg(not(target_env = "musl"))] {
+          windows::setup()
+        } else {
+          eprintln!("Cross compiling to windows-msvc is not supported from *-musl hosts")
+        }
+      }
+    }
     _ => {}
   }
 }
