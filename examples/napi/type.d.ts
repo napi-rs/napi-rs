f9c01b40f77f8f60c0cd2bdf0d16cf546745bfa5
LongYinan <lynweklm@gmail.com>
2021-09-23 01:44:36 +0800

    chore: publish 2.0.0-alpha.0

--- examples/napi/type.d.ts
+++ examples/napi/type.d.ts
@@ -2,8 +2,21 @@ export function getWords(): Array<string>
 export function getNums(): Array<number>
 export function sumNums(nums: Array<number>): number
 export function getCwd(callback: (arg0: string) => void): void
-export enum Kind { Dog = 0, Cat = 1, Duck = 2 }
-export enum CustomNumEnum { One = 1, Two = 2, Three = 3, Four = 4, Six = 6, Eight = 8, Nine = 9, Ten = 10 }
+export enum Kind {
+  Dog = 0,
+  Cat = 1,
+  Duck = 2,
+}
+export enum CustomNumEnum {
+  One = 1,
+  Two = 2,
+  Three = 3,
+  Four = 4,
+  Six = 6,
+  Eight = 8,
+  Nine = 9,
+  Ten = 10,
+}
 export function enumToI32(e: CustomNumEnum): number
 export function mapOption(val: number | undefined): number | undefined
 export function add(a: number, b: number): number
