29277308d7017c2e80c209e8656dc8c4659e7a12
LongYinan <lynweklm@gmail.com>
2021-10-31 13:13:21 +0800

    fix(napi-derive-backend): generate the same code if source was not changed

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -2,6 +2,8 @@ mod macos;
 mod windows;
 
 pub fn setup() {
+  println!("cargo:rerun-if-changed=TYPE_DEF_TMP_PATH");
+  println!("cargo:rerun-if-changed=DEBUG_GENERATED_CODE");
   match std::env::var("CARGO_CFG_TARGET_OS").as_deref() {
     Ok("macos") => macos::setup(),
     Ok("windows") => windows::setup(),
