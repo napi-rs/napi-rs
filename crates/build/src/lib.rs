ac34d0019c0a0ff9ae09c507550478e7c8ebb2b6
Sam Holmes <samholmes1337@gmail.com>
2021-11-22 11:17:46 +0000

    Fix cargo:rerun-if commands

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -2,8 +2,8 @@ mod macos;
 mod windows;
 
 pub fn setup() {
-  println!("cargo:rerun-if-changed=TYPE_DEF_TMP_PATH");
-  println!("cargo:rerun-if-changed=DEBUG_GENERATED_CODE");
+  println!("cargo:rerun-if-env-changed=TYPE_DEF_TMP_PATH");
+  println!("cargo:rerun-if-env-changed=DEBUG_GENERATED_CODE");
   match std::env::var("CARGO_CFG_TARGET_OS").as_deref() {
     Ok("macos") => macos::setup(),
     Ok("windows") => windows::setup(),
