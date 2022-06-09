f90d63ccde270357d517a4fe39643807515c8889
forehalo <forehalo@gmail.com>
2022-06-09 23:38:43 +0800

    fix(build): broken android target build caused by ndk upgrade

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -1,8 +1,13 @@
+mod android;
 mod macos;
 
 pub fn setup() {
   println!("cargo:rerun-if-env-changed=DEBUG_GENERATED_CODE");
-  if let Ok("macos") = std::env::var("CARGO_CFG_TARGET_OS").as_deref() {
-    macos::setup();
+  match std::env::var("CARGO_CFG_TARGET_OS").as_deref() {
+    Ok("macos") => {
+      macos::setup();
+    }
+    Ok("android") => if let Ok(_) = android::setup() {},
+    _ => {}
   }
 }
