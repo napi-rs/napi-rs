6ce49fa417a853387055c43cba5b35dc8668f466
Amr Bashir <amr.bashir2015@gmail.com>
2022-07-30 04:22:29 +0200

    use `CARGO_BUILD_TARGET_DIR` also if set

--- cli/src/build.ts
+++ cli/src/build.ts
@@ -409,7 +409,10 @@ export class BuildCommand extends Command {
       }
     }
 
-    const targetRootDir = process.env.CARGO_TARGET_DIR || await findUp(cwd)
+    const targetRootDir =
+      process.env.CARGO_TARGET_DIR ||
+      process.env.CARGO_BUILD_TARGET_DIR ||
+      (await findUp(cwd));
 
     if (!targetRootDir) {
       throw new TypeError('No target dir found')
