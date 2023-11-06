d2f2af4373b3245be22e3e90522b616d95c7fa62
LongYinan <lynweklm@gmail.com>
2023-11-06 12:07:10 +0800

    fix(build): rerun if cli version changed

--- crates/build/src/lib.rs
+++ crates/build/src/lib.rs
@@ -5,6 +5,7 @@ mod wasi;
 pub fn setup() {
   println!("cargo:rerun-if-env-changed=DEBUG_GENERATED_CODE");
   println!("cargo:rerun-if-env-changed=TYPE_DEF_TMP_PATH");
+  println!("cargo:rerun-if-env-changed=CARGO_CFG_NAPI_RS_CLI_VERSION");
   match std::env::var("CARGO_CFG_TARGET_OS").as_deref() {
     Ok("macos") => {
       macos::setup();
