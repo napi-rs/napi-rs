22c751ced0da1f9d33edea3dad141e9f37c5371f
LongYinan <lynweklm@gmail.com>
2024-04-18 00:14:00 +0800

    fix(napi-derive): bail the unexpected factory directive (#2051)
    - Close https://github.com/napi-rs/napi-rs/issues/2048

--- examples/napi/tests/build_error_tests/mod.rs
+++ examples/napi/tests/build_error_tests/mod.rs
@@ -1,5 +1,6 @@
 //! Include the test files here so they can be formatted properly with `cargo fmt`
 
+pub mod fn_outside_impl_factory;
 pub mod ts_arg_type_1;
 pub mod ts_arg_type_2;
 pub mod ts_arg_type_3;
