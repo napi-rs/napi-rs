a7ef9be9ca1ceb32f29116d76a32eb0306b6e270
LongYinan <lynweklm@gmail.com>
2024-07-06 14:11:21 +0800

    chore: external all dependencies in cli cjs build (#2166)

--- cli/esbuild.mjs
+++ cli/esbuild.mjs
@@ -1,11 +1,14 @@
 import * as esbuild from 'esbuild'
 
+import packageJson from './package.json' assert { type: 'json' }
+
 await esbuild.build({
   entryPoints: ['./dist/index.js'],
   outfile: './dist/index.cjs',
   bundle: true,
+  format: 'cjs',
   platform: 'node',
-  external: ['@napi-rs/lzma', '@napi-rs/tar', '@napi-rs/wasm-tools'],
+  external: Object.keys(packageJson.dependencies),
   define: {
     'import.meta.url': '__filename',
   },
