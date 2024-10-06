b951457e12c1dd701e3c2877a502a39ed64458a3
LongYinan <lynweklm@gmail.com>
2024-10-07 00:03:44 +0200

    refactor!(napi): remove clone fn on ThreadsafeFunction

--- examples/napi-compat-mode/__tests__/napi4/tsfn-dua-instance.js
+++ examples/napi-compat-mode/__tests__/napi4/tsfn-dua-instance.js
@@ -4,11 +4,9 @@ async function main() {
   await Promise.resolve()
   const a1 = new bindings.A((err, s) => {
     console.info(s)
-    a1.unref()
   })
   const a2 = new bindings.A((err, s) => {
     console.info(s)
-    a2.unref()
   })
   a1.call()
   a2.call()
