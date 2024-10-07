724673225ae9a4f17a6740a41a99f7abce532ae2
LongYinan <lynweklm@gmail.com>
2024-10-07 19:23:11 +0200

    refactor!(napi): remove clone fn on ThreadsafeFunction (#2291)

--- examples/napi-compat-mode/src/napi4/tsfn_dua_instance.rs
+++ examples/napi-compat-mode/src/napi4/tsfn_dua_instance.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use napi::{
   bindgen_prelude::Function, threadsafe_function::ThreadsafeFunction, CallContext, JsObject,
   JsUndefined,
@@ -6,15 +8,19 @@ use napi_derive::js_function;
 
 #[derive(Clone)]
 pub struct A {
-  pub cb: ThreadsafeFunction<String, napi::JsUnknown, String, false>,
+  pub cb: Arc<ThreadsafeFunction<String, napi::JsUnknown, String, false, true>>,
 }
 
 #[js_function(1)]
 pub fn constructor(ctx: CallContext) -> napi::Result<JsUndefined> {
   let callback = ctx.get::<Function<String>>(0)?;
 
-  let cb: ThreadsafeFunction<String, napi::JsUnknown, String, false> =
-    callback.build_threadsafe_function().build()?;
+  let cb: Arc<ThreadsafeFunction<String, napi::JsUnknown, String, false, true>> = Arc::new(
+    callback
+      .build_threadsafe_function()
+      .weak::<true>()
+      .build()?,
+  );
 
   let mut this: JsObject = ctx.this_unchecked();
   let obj = A { cb };
@@ -31,13 +37,5 @@ pub fn call(ctx: CallContext) -> napi::Result<JsUndefined> {
     "ThreadsafeFunction NonBlocking Call".to_owned(),
     napi::threadsafe_function::ThreadsafeFunctionCallMode::NonBlocking,
   );
-  ctx.env.get_undefined()
-}
-
-#[js_function]
-pub fn unref(ctx: CallContext) -> napi::Result<JsUndefined> {
-  let this = ctx.this_unchecked();
-  let obj = ctx.env.unwrap::<A>(&this)?;
-  obj.cb.unref(ctx.env)?;
   ctx.env.get_undefined()
 }
