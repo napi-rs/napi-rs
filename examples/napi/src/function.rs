915415214d1b6d5e3af99bd5750f4b7fe6d47ad7
Stanisław Czech <sc@lo3gdynia.pl>
2024-12-17 00:15:33 +0100

    [API BREAKING] Update api for calling functions.
    This commit introduces new type FnArgs,
    that is created with the goal of distinguishing between
    calling JS function from rust with tuple as argument
    and calling with multiple arguments.
    
    Currently there is no possibility to add ToNapiValue value,
    as tuple is used to call callback functions with more than one
    argument from rust.
    
    With this change, callbacks with multiple arguments,
    should be called, with converting tuple of argument
    into FnArgs struct, either by calling into() on such
    tuple, or by creating FnArgs struct directly.

--- examples/napi/src/function.rs
+++ examples/napi/src/function.rs
@@ -1,7 +1,7 @@
 #![allow(deprecated)]
 
 use napi::{
-  bindgen_prelude::{ClassInstance, Function, FunctionRef, PromiseRaw},
+  bindgen_prelude::{ClassInstance, FnArgs, Function, FunctionRef, PromiseRaw},
   threadsafe_function::ThreadsafeFunctionCallMode,
   Env, Error, Result, Status,
 };
@@ -19,8 +19,8 @@ pub fn call1(callback: Function<u32, u32>, arg: u32) -> Result<u32> {
 }
 
 #[napi]
-pub fn call2(callback: Function<(u32, u32), u32>, arg1: u32, arg2: u32) -> Result<u32> {
-  callback.call((arg1, arg2))
+pub fn call2(callback: Function<FnArgs<(u32, u32)>, u32>, arg1: u32, arg2: u32) -> Result<u32> {
+  callback.call((arg1, arg2).into())
 }
 
 #[napi]
@@ -43,8 +43,12 @@ pub fn call_function(cb: Function<(), u32>) -> Result<u32> {
 }
 
 #[napi]
-pub fn call_function_with_arg(cb: Function<(u32, u32), u32>, arg0: u32, arg1: u32) -> Result<u32> {
-  cb.call((arg0, arg1))
+pub fn call_function_with_arg(
+  cb: Function<FnArgs<(u32, u32)>, u32>,
+  arg0: u32,
+  arg1: u32,
+) -> Result<u32> {
+  cb.call((arg0, arg1).into())
 }
 
 #[napi(ts_return_type = "Promise<void>")]
@@ -75,18 +79,20 @@ pub fn call_function_with_arg_and_ctx(
 #[napi]
 pub fn reference_as_callback(
   env: Env,
-  callback: FunctionRef<(u32, u32), u32>,
+  callback: FunctionRef<FnArgs<(u32, u32)>, u32>,
   arg0: u32,
   arg1: u32,
 ) -> Result<u32> {
-  callback.borrow_back(&env)?.call((arg0, arg1))
+  callback.borrow_back(&env)?.call((arg0, arg1).into())
 }
 
 #[napi]
-pub fn build_threadsafe_function_from_function(callback: Function<(u32, u32), u32>) -> Result<()> {
+pub fn build_threadsafe_function_from_function(
+  callback: Function<FnArgs<(u32, u32)>, u32>,
+) -> Result<()> {
   let tsfn = callback.build_threadsafe_function().build()?;
   std::thread::spawn(move || {
-    tsfn.call((1, 2), ThreadsafeFunctionCallMode::NonBlocking);
+    tsfn.call((1, 2).into(), ThreadsafeFunctionCallMode::NonBlocking);
   });
   let tsfn_max_queue_size_1 = callback
     .build_threadsafe_function()
@@ -94,7 +100,7 @@ pub fn build_threadsafe_function_from_function(callback: Function<(u32, u32), u3
     .build()?;
 
   std::thread::spawn(move || {
-    tsfn_max_queue_size_1.call((1, 2), ThreadsafeFunctionCallMode::NonBlocking);
+    tsfn_max_queue_size_1.call((1, 2).into(), ThreadsafeFunctionCallMode::NonBlocking);
   });
 
   let tsfn_weak = callback
@@ -103,7 +109,7 @@ pub fn build_threadsafe_function_from_function(callback: Function<(u32, u32), u3
     .build()?;
 
   std::thread::spawn(move || {
-    tsfn_weak.call((1, 2), ThreadsafeFunctionCallMode::NonBlocking);
+    tsfn_weak.call((1, 2).into(), ThreadsafeFunctionCallMode::NonBlocking);
   });
 
   Ok(())
