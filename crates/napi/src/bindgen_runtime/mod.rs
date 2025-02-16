384dcac5888530838af2ac300ef5a52711ee4244
LongYinan <lynweklm@gmail.com>
2025-02-16 18:08:04 +0800

    chore(napi): remove thread_local! in REFERENCE_MAP (#2470)

--- crates/napi/src/bindgen_runtime/mod.rs
+++ crates/napi/src/bindgen_runtime/mod.rs
@@ -43,7 +43,7 @@ pub(crate) unsafe extern "C" fn raw_finalize_unchecked<T: ObjectFinalize>(
     return;
   }
   if let Some((_, ref_val, finalize_callbacks_ptr)) =
-    REFERENCE_MAP.with(|reference_map| reference_map.borrow_mut().remove(&finalize_data))
+    REFERENCE_MAP.borrow_mut(|reference_map| reference_map.remove(&finalize_data))
   {
     let finalize_callbacks_rc = unsafe { Rc::from_raw(finalize_callbacks_ptr) };
 
