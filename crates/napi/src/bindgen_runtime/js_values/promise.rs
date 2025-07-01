c1d98dc79980af4dacb0e9204a6f39a9c73ab316
LongYinan <lynweklm@gmail.com>
2025-07-01 22:24:51 +0800

    perf(napi): sender in Promise does not need to be Arc

--- crates/napi/src/bindgen_runtime/js_values/promise.rs
+++ crates/napi/src/bindgen_runtime/js_values/promise.rs
@@ -1,9 +1,11 @@
-use std::cell::Cell;
-use std::convert::identity;
-use std::future;
-use std::pin::Pin;
-use std::sync::Arc;
-use std::task::{Context, Poll};
+use std::{
+  cell::Cell,
+  convert::identity,
+  future,
+  pin::Pin,
+  rc::Rc,
+  task::{Context, Poll},
+};
 
 use tokio::sync::oneshot::{channel, Receiver};
 
@@ -60,7 +62,7 @@ impl<T: FromNapiValue> FromNapiValue for Promise<T> {
   unsafe fn from_napi_value(env: sys::napi_env, napi_val: sys::napi_value) -> crate::Result<Self> {
     let (tx, rx) = channel();
     let promise_object = unsafe { PromiseRaw::<T>::from_napi_value(env, napi_val)? };
-    let tx_box = Arc::new(Cell::new(Some(tx)));
+    let tx_box = Rc::new(Cell::new(Some(tx)));
     let tx_in_catch = tx_box.clone();
     promise_object
       .then(move |ctx| {
