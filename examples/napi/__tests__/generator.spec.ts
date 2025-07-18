69e6afba10941aee3d6b7ce2a1d224648b81f973
Mikaël Francoeur <mikael.francoeur@hotmail.com>
2025-07-18 09:29:50 -0400

    make the generator an iterator

--- examples/napi/__tests__/generator.spec.ts
+++ examples/napi/__tests__/generator.spec.ts
@@ -12,50 +12,76 @@ for (const [index, factory] of [
   () => new Fib3(0, 1),
 ].entries()) {
   test(`should be able to stop a generator #${index}`, (t) => {
-    const fib = factory()
-    const gen = fib[Symbol.iterator]
-    t.is(typeof gen, 'function')
-    const iterator = gen()
+    const iterator = factory()
+    // TODO remove these ignores when using the es2025 typescript target
+    // @ts-ignore
     t.deepEqual(iterator.next(), {
       done: false,
       value: 1,
     })
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     t.deepEqual(iterator.next(), {
       done: false,
       value: 8,
     })
+    // @ts-ignore
     t.deepEqual(iterator.return?.(), {
       done: true,
     })
+    // @ts-ignore
     t.deepEqual(iterator.next(), {
       done: true,
     })
   })
 
   test(`should be able to throw to generator #${index}`, (t) => {
-    const fib = factory()
-    const gen = fib[Symbol.iterator]
-    t.is(typeof gen, 'function')
-    const iterator = gen()
+    const iterator = factory()
+    // @ts-ignore
     t.deepEqual(iterator.next(), {
       done: false,
       value: 1,
     })
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     iterator.next()
+    // @ts-ignore
     t.deepEqual(iterator.next(), {
       done: false,
       value: 8,
     })
+    // @ts-ignore
     t.throws(() => iterator.throw!(new Error()))
+    // @ts-ignore
     t.deepEqual(iterator.next(), {
       done: true,
     })
+  })
+
+  test(`should be an Iterator and have the Iterator Helper methods #${index}`, (t) => {
+    const iterator = factory()
+
+    // @ts-ignore
+    t.true(Object.getPrototypeOf(iterator) === Iterator.prototype)
+    let arr = [
+      ...iterator
+        // @ts-ignore
+        .drop(3)
+        .filter((x: number) => x % 2 == 0)
+        .take(5),
+    ]
+    t.deepEqual(arr, [8, 34, 144, 610, 2584])
   })
 }
