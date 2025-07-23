2c53b865b51c9477d335a8cd8a9610ef5c68d446
LongYinan <lynweklm@gmail.com>
2025-07-23 15:34:29 +0800

    chore(cli): enforce the option values if user not provided

--- cli/src/commands/rename.ts
+++ cli/src/commands/rename.ts
@@ -1,8 +1,25 @@
+import { input } from '@inquirer/prompts'
+
 import { renameProject } from '../api/rename.js'
 import { BaseRenameCommand } from '../def/rename.js'
 
 export class RenameCommand extends BaseRenameCommand {
   async execute() {
+    const options = this.getOptions()
+    if (!options.name) {
+      const name = await input({
+        message: `Enter the new package name in the package.json`,
+        required: true,
+      })
+      options.name = name
+    }
+    if (!options.binaryName) {
+      const binaryName = await input({
+        message: `Enter the new binary name`,
+        required: true,
+      })
+      options.binaryName = binaryName
+    }
     await renameProject(this.getOptions())
   }
 }
