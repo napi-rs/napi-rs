daad12241e4ff5ca9770dd432bebd698dee427f5
Kevin Deng <sxzz@sxzz.moe>
2025-08-10 18:08:23 +0800

    build: build with tsdown (#2860)

---
+++ cli/tsdown.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig([
+  {
+    entry: './src/index.ts',
+    // remove in the future, release esm output only
+    format: ['esm', 'cjs'],
+    target: 'es2020',
+    sourcemap: 'inline',
+    inputOptions(options, format) {
+      if (format === 'cjs') {
+        options.external = ['@octokit/rest', 'lodash-es']
+      }
+      return options
+    },
+  },
+  {
+    entry: './src/cli.ts',
+    sourcemap: 'inline',
+    target: 'es2020',
+    dts: false,
+  },
+])
