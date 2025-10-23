01485ff4231395b0f39d7bbd9b8053992f9d3923
LongYinan <lynweklm@gmail.com>
2025-10-23 14:18:30 +0800

    fix(napi): do not invoke process.report on Windows (#2972)

--- cli/src/api/templates/js-binding.ts
+++ cli/src/api/templates/js-binding.ts
@@ -146,7 +146,7 @@ function requireNative() {
     }
   } else if (process.platform === 'win32') {
     if (process.arch === 'x64') {
-      if (process.report?.getReport?.()?.header?.osName?.startsWith?.('MINGW')) {
+      if (process.config?.variables?.shlib_suffix === 'dll.a' || process.config?.variables?.node_target_type === 'shared_library') {
         ${requireTuple('win32-x64-gnu')}
       } else {
         ${requireTuple('win32-x64-msvc')}
