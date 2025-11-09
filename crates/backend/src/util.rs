16725abf21dd89c9bee253e93dca153f60856f43
LongYinan <lynweklm@gmail.com>
2025-11-09 22:20:56 +0800

    avoid breaking change

--- crates/backend/src/util.rs
+++ crates/backend/src/util.rs
@@ -3,7 +3,7 @@ use convert_case::{Case, Casing};
 pub fn to_case<T: AsRef<str>>(input: T, case: Case<'static>) -> String {
   if input.as_ref().starts_with('_') {
     let trimmed = input.as_ref().trim_start_matches('_');
-    format!("_{}", trimmed.to_case(case))
+    trimmed.to_case(case)
   } else {
     input.as_ref().to_case(case)
   }
