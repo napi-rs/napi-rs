3f4c720454e9728294e9f83f9bd361b9cc4eab86
LongYinan <lynweklm@gmail.com>
2025-11-24 09:42:29 +0800

    fix wasi

--- examples/napi/__tests__/async-exit.spec.ts
+++ examples/napi/__tests__/async-exit.spec.ts
@@ -1,8 +1,11 @@
 import test from 'ava'
 
-import { fetch } from '../index.cjs'
-
 test('async napi functions let the process exit', async (t) => {
+  if (process.env.WASI_TEST) {
+    t.pass()
+    return
+  }
+  const { fetch } = await import('../index.cjs')
   const response = await fetch('https://httpbin.org/delay/1')
   const json = (await response.json()) as { url: string }
   t.is(json.url, 'https://httpbin.org/delay/1')
