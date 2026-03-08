ecec35d349aab794c10185d22c1acd3e648a5d54
jeiea <solum5013@gmail.com>
2026-03-08 15:57:14 +0900

    fix(napi): propagate get_type error in extract_error_cause

--- crates/napi/src/error.rs
+++ crates/napi/src/error.rs
@@ -675,7 +675,7 @@ macro_rules! check_pending_exception {
 }
 
 fn extract_error_cause(value: Unknown<'_>) -> Result<Option<Box<Error>>> {
-  if !matches!(value.get_type(), Ok(ValueType::Object)) {
+  if value.get_type()? != ValueType::Object {
     return Ok(None);
   }
 
