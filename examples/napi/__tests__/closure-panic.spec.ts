25d70abe1f74669091a3660ed6f06df4a9c69136
jerry4718 <jerry4718@qq.com>
2026-08-30 01:56:37 +0800

    fix(napi-example): import closure-panic spec bindings from index.cjs
    Align with the other specs: the generated type declarations live in
    index.d.cts, so importing '../index' fails type-checking with TS2307.

--- examples/napi/__tests__/closure-panic.spec.ts
+++ examples/napi/__tests__/closure-panic.spec.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import { createPanickingClosureFunction } from '../index'
+import { createPanickingClosureFunction } from '../index.cjs'
 
 test('create_function_from_closure: panic in the closure becomes a JS error', (t) => {
   const data = createPanickingClosureFunction()
